56f24e07a7abc756bf635129190e064c35f5e681
gkpole <89931423+gkpole@users.noreply.github.com>
2022-01-21 22:43:11 +0500

    Update Demot.py

--- Demot.py
+++ Demot.py
@@ -19,7 +19,7 @@ class DeMoTiVaToRsMod(loader.Module):
 		self.client = client
 	
 	
-	@loader.tds
+	@loader.owner
 	async def demoticmd(self, message):
 		"""текст + фото или ответ на фото
            не мнёт фотки"""
